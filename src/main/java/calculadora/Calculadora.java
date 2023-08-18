ab4ad6f1d6867e18029cfb107a9b4bc083b000be
amontufar1 <amontufara1@miumg.edu.gt>
2023-08-18 00:02:09 -0600

    primer commit

---
+++ src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculadora;
+
+/**
+ *
+ * @author User
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        calculadorai calculadorai = new calculadorai();
+                calculadorai.show();
+        
+    }
+}
